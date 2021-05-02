c1e5593c9ea5997c5b14484ee98a111edaca2cf3
cheasles <cheasles@github.com>
2021-05-02 21:17:11 +1200

    Ran pdk convert.

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -3,23 +3,18 @@ require 'spec_helper_acceptance'
 describe 'restic class' do
   context 'default parameters' do
     # Using puppet_apply as a helper
-    it 'should work idempotently with no errors' do
+    it 'work idempotently with no errors' do
       pp = <<-EOS
       class { 'restic': }
       EOS
 
       # Run it twice and test for idempotency
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes  => true)
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
     end
 
     describe package('restic') do
       it { is_expected.to be_installed }
-    end
-
-    describe service('restic') do
-      it { is_expected.to be_enabled }
-      it { is_expected.to be_running }
     end
   end
 end
--- spec/classes/restic_spec.rb
+++ spec/classes/restic_spec.rb
@@ -8,31 +8,14 @@ describe 'restic' do
           facts
         end
 
-        context "restic class without any parameters" do
+        context 'restic class without any parameters' do
           it { is_expected.to compile.with_all_deps }
 
-          it { is_expected.to contain_class('restic::params') }
-          it { is_expected.to contain_class('restic::install').that_comes_before('restic::config') }
-          it { is_expected.to contain_class('restic::config') }
-          it { is_expected.to contain_class('restic::service').that_subscribes_to('restic::config') }
+          it { is_expected.to contain_class('restic::install') }
 
-          it { is_expected.to contain_service('restic') }
-          it { is_expected.to contain_package('restic').with_ensure('present') }
+          it { is_expected.to contain_package('restic').with_ensure('latest') }
         end
       end
-    end
-  end
-
-  context 'unsupported operating system' do
-    describe 'restic class without any parameters on Solaris/Nexenta' do
-      let(:facts) do
-        {
-          :osfamily        => 'Solaris',
-          :operatingsystem => 'Nexenta',
-        }
-      end
-
-      it { expect { is_expected.to contain_package('restic') }.to raise_error(Puppet::Error, /Nexenta not supported/) }
     end
   end
 end
